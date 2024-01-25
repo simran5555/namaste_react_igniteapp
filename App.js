09bad42f2d0c20bb8ed6ddf3a54f858ef86f1349
simran5555 <simran.kaur@ayasya.com>
2024-01-25 12:37:07 +0530

    final edits

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 
-const parent = React.createElement('h1', { id: 'parent' }, "hello from react")
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child1', key:1 }, [
+    React.createElement('h1', {key:1}, "h1 tag"),
+    React.createElement('h2', {key:2}, "h2 tag")
+  ]),
+  React.createElement('div', { id: 'child2', key:2 }, [
+    React.createElement('h1', {key:1}, "h1 tag"),
+    React.createElement('h2', {key:2}, "h2 tag")
+  ])
+])
 
-  const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-  root.render(parent)
+root.render(parent)
 
-  //package.json is configuration for npm
+//package.json is configuration for npm
